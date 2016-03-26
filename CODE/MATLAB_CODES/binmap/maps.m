9aa16d4bd574556803f4c280892e4687a57356a3
Ashima Jain <ashimajain2595@gmail.com>
2016-03-26 12:42:49 +0530

    maps added

--- CODE/MATLAB_CODES/binmap/maps.m
+++ CODE/MATLAB_CODES/binmap/maps.m
@@ -1,18 +1,18 @@
-for i = 77:79
+for i = 3:3
     file = strcat(int2str(i),'_tl.mat');
     load(file);
     S = zeros(size(bin_map));
     G = S;
     S = S | bin_map;
-    file = strcat(int2str(i),'_tr.mat');
-    load(file);
-    S = S | bin_map;
+%     file = strcat(int2str(i),'_tr.mat');
+%     load(file);
+%     S = S | bin_map;
     file = strcat(int2str(i),'_bl.mat');
     load(file);
     S = S | bin_map;
-    file = strcat(int2str(i),'_br.mat');
-    load(file);
-    S = S | bin_map;
+%     file = strcat(int2str(i),'_br.mat');
+%     load(file);
+%     S = S | bin_map;
 %     figure;
 %     imshow(S)
     file = strcat(int2str(i),'.bmp');
@@ -20,14 +20,16 @@ for i = 77:79
     
     file = strcat(int2str(i),'_tlbin.mat');
     load(file);
-    G = G | bin_map;file = strcat(int2str(i),'_trbin.mat');
-    load(file);
-    G = G | bin_map;file = strcat(int2str(i),'_blbin.mat');
+    G = G | bin_map;
+%     file = strcat(int2str(i),'_trbin.mat');
+%     load(file);
+%     G = G | bin_map;
+    file = strcat(int2str(i),'_blbin.mat');
     load(file);
     G = G | bin_map;
-    file = strcat(int2str(i),'_brbin.mat');
-    load(file);
-    G = G | bin_map;
+%     file = strcat(int2str(i),'_brbin.mat');
+%     load(file);
+%     G = G | bin_map;
 %     figure;
 %     imshow(G)
     file = strcat(int2str(i),'_bin.bmp');

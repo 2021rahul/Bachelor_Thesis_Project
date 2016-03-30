d0cddfc9bd888858b3b95661cc6d7badaf6d9e2c
2021rahul <rahulghosh2021@gmail.com>
2016-03-30 21:58:37 +0530

    rgb+he

--- Exp1_HE/untitled0.py
+++ Exp1_HE/untitled0.py
@@ -12,45 +12,55 @@ import scipy.io
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 srng = RandomStreams()
-def load_data(xdirname,ydirname):
+def load_data(xdirname , ydirname):
     print '...loading data'
     
     mat = []
-    included_extensions = ['mat']
-    filelist = [fn[:-6] for fn in os.listdir(xdirname) if any([fn.endswith(ext) for ext in included_extensions])]
-    filelist = list(set(filelist))
+    
+    included_extensions = ['bmp']
+    filelist = [fn[:-9] for fn in os.listdir(xdirname) if any([fn.endswith(ext) for ext in included_extensions])]
+    iext = '_norm.bmp'
     hext = '_H.mat'
     eext = '_E.mat'
     aext = '_bin.bmp'
+    
     for fname in filelist:
         
         xname = xdirname + fname
+        iname = xname + iext
         hname = xname + hext
         ename = xname + eext
         aname = ydirname + fname + aext
-        
+
         H = scipy.io.loadmat(hname)
         H = H['H']
         E = scipy.io.loadmat(ename)
         E = E['E']
-#        print H.shape
-        
+
+        xim = Image.open(xname)
+        xpix = xim.load()
         yim = Image.open(aname)
+        ypix = yim.load()
+
         width,height = yim.size
-        ypix = yim.load() 
+        red = np.zeros((height,width))
+        green = np.zeros((height,width))
+        blue = np.zeros((height,width))
         fgrnd = np.zeros((height,width))
-#        print fgrnd.shape
         
         for x in range(width):
             for y in range(height):
+                red[y,x] = xpix[x,y][0]
+                green[y,x] = xpix[x,y][1]
+                blue[y,x] = xpix[x,y][2]
                 if ypix[x,y]:
                     fgrnd[y,x] = 1.0
 
-        img=np.array([H,E,fgrnd])
+        img=np.array([red,green,blue,H,E,fgrnd])
         mat.append(img)
         
     return mat
-
+    
 trainxdirname = '/media/rahul/1098D6BA98D69E12/academics/SEMESTER_7/BTP/Bachelor_Thesis_Project/Exp1_HE/Train/'
 trainydirname = '/media/rahul/1098D6BA98D69E12/academics/SEMESTER_7/BTP/Bachelor_Thesis_Project/Exp1_HE/Train_anno/'
 

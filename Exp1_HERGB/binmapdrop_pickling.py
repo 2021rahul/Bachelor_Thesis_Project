511028d0bd5b38c22e501718f8806123de1e03d3
ashimajain2595 <ashimajain2595@gmail.com>
2016-04-07 09:17:56 +0530

    code changes

--- Exp1_HERGB/binmapdrop_pickling.py
+++ Exp1_HERGB/binmapdrop_pickling.py
@@ -37,7 +37,7 @@ def load_data(xdirname , ydirname):
         E = scipy.io.loadmat(ename)
         E = E['E']
 
-        xim = Image.open(xname)
+        xim = Image.open(iname)
         xpix = xim.load()
         yim = Image.open(aname)
         ypix = yim.load()
